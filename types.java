e131f93ad9f52ca2406202f530e8ca9d3c31ca26
ddfunk2 <77017235+ddfunk2@users.noreply.github.com>
2021-02-06 17:36:49 -0500

    Java variables

---
+++ types.java
@@ -0,0 +1,37 @@
+#We can start with an int, a variable that hold just numbers (no decimals)
+
+int myInt = 45;
+
+#Remember, every java line of code (except for loops) have a ; at the end.
+#We can also make a double, a number that can have decimals.
+
+float myFloat = 3.1415f;
+
+#Make sure you have the f at the end. This helps Java figure out which type of decimal variable you are creating. 
+#The other type of decimal variable is a double. It can hold more decimals. You do not need an f
+
+double myDouble = 3.1415926;
+
+#You might not want a number, but a character (4, f, @, ', etc)...
+
+char myChar = '3';
+
+#myChar is a Character.
+
+#A string is multiple characters.
+
+String myString = "ddfunk2";
+
+#After we have all these variables, we want to print them out! 
+
+#You can print something like this:
+
+System.out.println(myDouble);
+#Remember the ;
+#If you want to print something without going to the next line (what ln does), you can remove the ln
+
+System.out.print(myDouble);
+#If you want to print out something without making a variable, feel free to use quotations!
+
+System.out.println("Hi! My name is ddfunk2.");
+#I almost forgot the ;
